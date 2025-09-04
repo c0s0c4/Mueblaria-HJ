9aa911b0ecbae1d6f8c5b8aaa5dc6efae57577d8
milenacas <122313670+milenacas@users.noreply.github.com>
2025-09-04 11:44:19 +0000

    Thu, Sep 4, 2025, 8:44 AM -03:00

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // Lista de productos en el buscador
-document.addEventListener
+<script>
+  const input = document.getElementById("input")
+  const lista = document.getElementById("listadeproductos")
+  const items = lista.getElementsByTagName("li")
+
+  input.addEventListener("input", function() {
+    const filtro = input.value.toLowerCase()
+
+    for (let i = 0; i < items.length; i++) {
+      const texto = items[i].textContent.toLowerCase()
+      if (texto.includes(filtro)) {
+        items[i].style.display = "" // mostrar
+      } else {
+        items[i].style.display = "none"// ocultar
+      }
+    }
+  })
+</script>
