dfcf1e851d72036a0557cd7fada6b52a2d1fdd1a
milenacas <122313670+milenacas@users.noreply.github.com>
2025-09-04 12:35:29 +0000

    Thu, Sep 4, 2025, 9:35 AM -03:00

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-
 const input = document.getElementById("input");
 const lista = document.getElementById("listadeproductos");
 const items = lista.getElementsByTagName("li");
 
 input.addEventListener("input", function() {
-  const filtro = input.value.toLowerCase();
-  let tieneCoincidencias = false;
+    const filtro = input.value.toLowerCase();
+    let tieneCoincidencias = false;
 
-  for (let i = 0; i < items.length; i++) {
-    const texto = items[i].textContent.toLowerCase();
-    if (texto.includes(filtro) && filtro !== "") {
-      items[i].style.display = "block";
-      tieneCoincidencias = true;
-    } else {
-      items[i].style.display = "none";
+    for (let i = 0; i < items.length; i++) {
+        const texto = items[i].textContent.toLowerCase();
+        if (texto.includes(filtro) && filtro !== "") {
+            items[i].style.display = "block";
+            tieneCoincidencias = true;
+        } else {
+            items[i].style.display = "none";
+        }
     }
-  }
 
-  lista.style.display = tieneCoincidencias ? "block" : "none";
+    lista.style.display = tieneCoincidencias ? "block" : "none";
 });
