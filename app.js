87caf9c96b1af39fd7fa66e62b9be59f1defacea
milenacas <122313670+milenacas@users.noreply.github.com>
2025-09-07 01:27:08 -0300

    carrito y conexiones

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-
 const contenedor = document.getElementById("productos-container");
 
 productos.forEach(p => {
-  const card = document.createElement("div")
-  card.classList.add("card")
+    const card = document.createElement("div")
+    card.classList.add("card")
 
-  card.innerHTML = `
+    card.innerHTML = `
     <img src="${p.imagen}" alt="${p.nombre}" width="200">
     <div class="card-content">
       <h2 class="nombreproductocard">${p.nombre}</h2>
+      <button class="btn btnDetalle" data-id="${p.id}">Detalle de producto</button>
       <p class="precio">${p.precio}</p>
-      <button id="Detalle del producto" class="btn">Detalle del producto</button>
     </div>
   `
-  contenedor.appendChild(card)
+    contenedor.appendChild(card)
 })
 
-document.getElementById("Detalle del producto").addEventListener("click", function() {
-    window.location.href = "producto.html";
-});
-
-
-
-  let contador = 0;
-
-  const botones = document.querySelectorAll('.btnAgregarCarrito');
-        const contadorCarrito = document.getElementById('contadorCarrito');
-
-
-        botones.forEach(boton => {
-            boton.addEventListener('click', () => {
-                contador++; // Incrementa contador
-                contadorCarrito.textContent = contador  
-                alert("Producto agregado al carrito")
-            })
-        })
-
+// redirección al detalle
+document.querySelectorAll(".btnDetalle").forEach(boton => {
+    boton.addEventListener("click", function() {
+        const id = this.getAttribute("data-id")
+        window.location.href = `producto.html?id=${id}`
+    })
+})
