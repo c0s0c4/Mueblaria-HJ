fe7ccb4b1d6de2f8feb250bae1e338ac52c7a6ca
milenacas <122313670+milenacas@users.noreply.github.com>
2025-09-07 17:24:41 -0300

    contacto

--- app.js
+++ app.js
@@ -22,3 +22,25 @@ document.querySelectorAll(".btnDetalle").forEach(boton => {
         window.location.href = `producto.html?id=${id}`
     })
 })
+
+
+
+// contacto
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
