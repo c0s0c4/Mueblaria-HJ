d72f133472435508b4531a211747d9bbf86c0b9c
milenacas <122313670+milenacas@users.noreply.github.com>
2025-10-05 21:20:44 +0000

    Sun, Oct 5, 2025, 6:20 PM -03:00

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { productos } from "/home/ubuntu/client/client/src/productosDetalle.js"; // importar productos
-import "/home/ubuntu/client/client/src/CSS/Detalle.css";
-import "/home/ubuntu/client/client/src/CSS/Menu.css";
+import { productos } from "/home/ubuntu/Mueblaria-HJ-6/client/src/productosDetalle.js"; // importar productos
+import "/home/ubuntu/Mueblaria-HJ-6/client/src/CSS/Detalle.css";
+import "/home/ubuntu/Mueblaria-HJ-6/client/src/CSS/Menu.css";
 
 function DetalleProducto() {
   const { productoKey } = useParams();
