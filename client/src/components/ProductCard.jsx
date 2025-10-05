bd4d8721f3baea5feed85b13ef8ee3f7ef76d047
milenacas <122313670+milenacas@users.noreply.github.com>
2025-10-05 21:35:30 +0000

    Sun, Oct 5, 2025, 6:35 PM -03:00

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import "/home/ubuntu/Mueblaria-HJ-6/client/src/CSS/ProductosCard.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { productos } from "/home/ubuntu/Mueblaria-HJ-6/client/src/productos.js";
+import { productos } from "./Mueblaria-HJ-6/client/src/productos.js";
 import "/home/ubuntu/Mueblaria-HJ-6/client/src/CSS/Menu.css";
 
 function ProductosCard() {
