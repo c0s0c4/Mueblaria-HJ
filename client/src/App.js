79efb953344e7b1df5c202a8b699701fd69f5a72
milenacas <122313670+milenacas@users.noreply.github.com>
2025-10-05 21:30:11 +0000

    Sun, Oct 5, 2025, 6:30 PM -03:00

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import ProductList from "./components/ProductList";
 import Navbar from "./components/Navbar";
 import Home from './components/Home.jsx';
 import Nosotros from './components/Nosotros.jsx';
