197c737f113c3c9ec471ea4de30924d7aa508f97
Milena Casarrubia <122313670+milenacas@users.noreply.github.com>
2025-10-11 16:56:22 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const productosRoutes = require('./routes/productosRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // ========================================
 // MIDDLEWARES GLOBALES
