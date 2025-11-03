cc7031818967ee95c1842810caf4a3d5a140fc30
Milena <casarrubiamilena148@gmail.com>
2025-11-02 21:10:11 -0300

    front y back update

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    index: true
+  },
   nombre: {
     type: String,
     required: [true, 'El nombre del producto es obligatorio'],
